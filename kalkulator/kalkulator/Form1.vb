cbdb9b8c4d85bd28914f90dcd8bcae593865ac4a
ilhamagung <ilhamagungp6196@gmail.com>
2015-10-10 22:03:32 +0700

    kalkulator
    tugas 2

---
+++ kalkulator/kalkulator/Form1.vb
@@ -0,0 +1,77 @@
+﻿Public Class Form1
+        Const phi As Double = 22 / 7
+        Dim input_angka, Hasil As Double
+        Dim Rumus As String
+
+        Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox1.Text = TextBox1.Text & "7"
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox1.Text = TextBox1.Text & "8"
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        TextBox1.Text = TextBox1.Text & "9"
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        TextBox1.Text = TextBox1.Text & "4"
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        TextBox1.Text = TextBox1.Text & "5"
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        TextBox1.Text = TextBox1.Text & "6"
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        TextBox1.Text = TextBox1.Text & "1"
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        TextBox1.Text = TextBox1.Text & "2"
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        TextBox1.Text = TextBox1.Text & "3"
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        TextBox1.Text = TextBox1.Text & "0"
+    End Sub
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        input_angka = Val(TextBox1.Text)
+        Rumus = "+"
+        TextBox1.Text = Nothing
+    End Sub
+
+    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles Button13.Click
+        input_angka = Val(TextBox1.Text)
+        Rumus = "-"
+        TextBox1.Text = Nothing
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        If Rumus = "+" Then
+            TextBox1.Text = input_angka + Val(TextBox1.Text)
+        ElseIf Rumus = "-" Then
+            TextBox1.Text = input_angka - Val(TextBox1.Text)
+        End If
+
+    End Sub
+
+    Private Sub Button14_Click(sender As Object, e As EventArgs) Handles Button14.Click
+        TextBox1.Text = Nothing
+        input_angka = Nothing
+        Hasil = Nothing
+        Rumus = Nothing
+    End Sub
+End Class
